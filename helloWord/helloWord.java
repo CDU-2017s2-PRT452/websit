f25eea5f8341be197471010eec6e90d79f508d32
JerryKang666 <30918020+JerryKang666@users.noreply.github.com>
2017-08-11 13:53:49 +0930

    Jerry
    1234

---
+++ helloWord/helloWord.java
@@ -0,0 +1,14 @@
+package helloWord;
+
+public class helloWord {
+
+	private static char[] HelloWord;
+
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+System.out.print("helloWord");
+System.out.println("1234");
+System.out.println("555555666666");
+	}
+
+}
